2103b2316747ec817b5920a98b534ef49acacb71
popCail <pop255huaha@gmail.com>
2023-04-21 22:00:54 +0800

    增加两张视错觉图片

---
+++ js/tiangang36js/11.js
@@ -0,0 +1,86 @@
+drawB();
+
+
+function drawB() {
+    var c = document.getElementById('canvas2');
+    var ctx = c.getContext('2d');
+    var width = 1000;
+    var height = 800;
+    var R = 20;
+    c.width = width;
+    c.height = height;
+
+    ctx.fillStyle = '#b9e600';
+    ctx.fillRect(0, 0, width, height);
+
+    ctx.fillStyle = '#9e02fb';
+    for (let i = 5 * R / 4; i < width; i += R * 5 / 2) {
+        for (let j = 5 * R / 4; j < height; j += R * 7 / 3) {
+            ctx.beginPath();
+            ctx.arc(i, j, R, 0, 2 * Math.PI, false);
+            ctx.fill();
+        }
+    }
+    var N = 8;
+    ctx.lineWidth = R / N;
+    for (let i = 5 * R / 4; i < width; i += R * 5 / 2) {
+        for (let j = 5 * R / 4; j < height; j += R * 7 / 3) {
+            let randomArc = Math.random() * 2 * Math.PI;
+
+            ctx.strokeStyle = '#ffffff';
+            ctx.beginPath();
+            ctx.arc(i, j, R*(N-1/2)/N,randomArc , randomArc + Math.PI, false);
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.strokeStyle = '#000000';
+            ctx.arc(i, j, R*(N-1/2)/N,randomArc + Math.PI , randomArc + 2 * Math.PI, false);
+            ctx.stroke();
+        }
+    }
+}
+
+
+setInterval(()=>{
+    cirArc += 0.01;
+   drawA();
+},20);
+var cirR = 2;
+var cirArc = Math.PI/3;
+
+function drawA() {
+    var c = document.getElementById('canvas1');
+    var ctx = c.getContext('2d');
+    var width = 1000;
+    var height = 800;
+    var lineWidth = 15;
+    c.width = width;
+    c.height = height;
+
+    ctx.fillStyle = '#040404';
+    ctx.fillRect(0, 0, width, height);
+    ctx.fill();
+
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = '#7d7d7d';
+    ctx.beginPath();
+    for (let i = lineWidth * 3; i < width; i += 4 * lineWidth) {
+        ctx.moveTo(i, 0);
+        ctx.lineTo(i, height);
+    }
+    for (let i = lineWidth * 3; i < height; i += 4 * lineWidth) {
+        ctx.moveTo(0, i);
+        ctx.lineTo(width, i);
+    }
+    ctx.stroke();
+
+    ctx.fillStyle = '#ffffff';
+
+    for (let i = lineWidth * 3 ; i < width; i += 4 * lineWidth) {
+        for (let j = lineWidth * 3 ; j < height; j += 4 * lineWidth) {
+            // ctx.fillRect(i, j, lineWidth, lineWidth);
+            ctx.beginPath();
+            ctx.arc(i + Math.cos(cirArc) * cirR, j + Math.sin(cirArc) * cirR , lineWidth / 2, 0, 2 * Math.PI, false);
+            ctx.fill();
+        }
+    }
+}
